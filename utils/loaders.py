4af45251bb3bda1f25240d826ae14c883caf9bb5
386er <t.burri@mailbox.org>
2020-08-03 00:35:52 +0200

    Update loader

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,25 +1,6 @@
 import pickle
 import os
 
-#from keras.datasets import mnist, cifar100,cifar10
-#from keras.preprocessing.image import ImageDataGenerator, load_img, save_img, img_to_array
-
-#import pandas as pd
-
-#import numpy as np
-#from os import walk, getcwd
-#import h5py
-
-#import scipy
-#from glob import glob
-
-#from keras.applications import vgg19
-#from keras import backend as K
-#from keras.utils import to_categorical
-
-#import pdb
-
-
 
 
 def load_model(model_class, folder):
